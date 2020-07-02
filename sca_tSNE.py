a79648fda72f7f5b63ed8b9485bae07f7c495b82
unknown <sstreib@student.ethz.ch>
2020-07-02 18:46:16 +0200

    tSNE finalized, plot label updated, added kl divergence into title

--- sca_tSNE.py
+++ sca_tSNE.py
@@ -44,25 +44,24 @@ genes.remove("")
 # %%  for local execution (remove for full picture)
 
 
-print(datetime.now().strftime("%H:%M:%S>"), "deleting random data pieces...")
-genes_uplimit = 30000
-genes_downlimit = 25000
-cells_uplimit = 25000
-cells_downlimit = 10000
+# print(datetime.now().strftime("%H:%M:%S>"), "deleting random data pieces...")
+# genes_uplimit = 30000
+# genes_downlimit = 25000
+# cells_uplimit = 25000
+# cells_downlimit = 10000
 
-# prev_element = "gulligulli"
-# for index in range(len(labels)):
-#     if labels[index] != prev_element:
-#         print(index)
-#     prev_element = labels[index]
-coomatrix = data #so that it is transposed, sorry for uglyness, this shouldn't be visible to outside. 
-labels = labels[cells_downlimit:cells_uplimit]
+# # prev_element = "gulligulli"
+# # for index in range(len(labels)):
+# #     if labels[index] != prev_element:
+# #         print(index)
+# #     prev_element = labels[index]
+# coomatrix = data #so that it is transposed, sorry for uglyness, this shouldn't be visible to outside. 
+# labels = labels[cells_downlimit:cells_uplimit]
 
-reduced = coomatrix.tocsr()
-data = reduced[cells_downlimit:cells_uplimit, genes_downlimit:genes_uplimit]
+# reduced = coomatrix.tocsr()
+# data = reduced[cells_downlimit:cells_uplimit, genes_downlimit:genes_uplimit]
 
-
-genes = genes[genes_downlimit:genes_uplimit]
+# genes = genes[genes_downlimit:genes_uplimit]
 
 
 # %%
@@ -72,28 +71,30 @@ data = StandardScaler(with_mean= False).fit_transform(data) # Standardizing the 
 
 # %%
 
-dfasdfsfd
+
 print(datetime.now().strftime("%H:%M:%S>"), "Calculating tSNE...")
-tsne = TSNE(n_components=2, verbose = 3)
+tsne = TSNE(n_components=2, verbose = 10)
 tsnedata = tsne.fit_transform(data)
+
+
+embed = tsne.embedding_
+kldiver = tsne.kl_divergence_
+niter = tsne.n_iter_
 
 
 
 
 #%% Outputs
 
-output_dir = "./scaIsomap_output"
-component_name = "Isomap"
+output_dir = "./scaTSNE_output"
+component_name = "t-SNE"
 
 
 
 
 # construct dataframe for 2d plot
-df = pd.DataFrame(data = reduced[:,[0,1]], columns = [ component_name + ' 1', component_name + ' 2'])
+df = pd.DataFrame(data = tsnedata[:,[0,1]], columns = [ component_name + ' 1', component_name + ' 2'])
 df['celltlype'] = labels
-
-
-
 
 
 
@@ -109,9 +110,11 @@ targets = set(labels) # what it will draw in plot, previously it was targets = [
 
 fig = plt.figure(figsize = (8,8))
 ax = fig.add_subplot(1,1,1) 
-ax.set_xlabel(component_name + ' 1 (' + str(round(explained_variance[0]*100, 3)) + "% of variance)", fontsize = 15)
-ax.set_ylabel(component_name + ' 2 (' + str(round(explained_variance[1]*100, 3)) + "% of variance)", fontsize = 15)
-ax.set_title('Most Powerful '+ component_name +'s', fontsize = 20)
+ax.set_xlabel(component_name + " 1", fontsize = 15)
+ax.set_ylabel(component_name + " 2", fontsize = 15)
+#ax.set_xlabel(component_name + ' 1 (' + str(round(explained_variance[0]*100, 3)) + "% of variance)", fontsize = 15)
+#ax.set_ylabel(component_name + ' 2 (' + str(round(explained_variance[1]*100, 3)) + "% of variance)", fontsize = 15)
+ax.set_title(component_name +' Plot (KL-divergence = ' + str(round(tsne.kl_divergence_, 2)) + ')', fontsize = 20)
 colors = cm.rainbow(np.linspace(0, 1, len(targets)))
 for target, color in zip(targets,colors):
     indicesToKeep = df['celltlype'] == target
@@ -121,83 +124,10 @@ for target, color in zip(targets,colors):
                , s = 5)
 ax.legend(targets)
 ax.grid()
-plt.savefig(output_dir + "/PCA_result.png")
+plt.savefig(output_dir + "/tSNE_Plot.png")
 
 
 
-
-
-### Save Variances
-print(datetime.now().strftime("%H:%M:%S>"), "saving explained variances...")
-explained_sum = np.cumsum(explained_variance)
-
-file = open(output_dir + '/explained_variances.log', 'w')
-for i in range(len(explained_variance)):
-    text = (str(i + 1) + "\t" + str(explained_variance[i]) + "\t" + str(explained_sum[i]) + "\n")
-    file.write(text)
-file.close()
-    
-    
-    
-    
-    
-### Scree Plots
-how_many = -1;
-
-perc_var = (explained_variance * 100)
-perc_var = perc_var[0:how_many]
-
-labelz = [str(x) for x in range(1, len(perc_var)+1)]
-
-
-plt.figure(figsize=[16,8])
-plt.bar(x = range(1, len(perc_var)+1), height = perc_var, tick_label = labelz)
-plt.ylabel('Percentage of explained variance')
-plt.xlabel('Principal component')
-plt.title('Scree plot')
-plt.show()    
-plt.savefig(output_dir + "/PCA_scree_plot_all.png")
-    
-    
-    
-    
-how_many = 50;
-
-perc_var = (explained_variance * 100)
-perc_var = perc_var[0:how_many]
-
-labelz = [str(x) for x in range(1, len(perc_var)+1)]
-
-
-plt.figure(figsize=[16,8])
-plt.bar(x = range(1, len(perc_var)+1), height = perc_var, tick_label = labelz)
-plt.ylabel('Percentage of explained variance')
-plt.xlabel('Principal component')
-plt.title('Scree plot')
-plt.show()    
-plt.savefig(output_dir + "/PCA_scree_plot_top50.png")    
-    
-    
-    
-    
-    
-# Loading scores for PC1
-
-
-how_many = 10
-
-loading_scores = pd.Series(myPCA.components_[0], index = genes)
-sorted_loading_scores = loading_scores.abs().sort_values(ascending=False)
-top_genes = sorted_loading_scores[0:how_many].index.values
-    
-
-file = open(output_dir + '/most_important_genes.log', 'w')
-for i in range(how_many):
-    text = (str(top_genes[i]) + "\t" + str(sorted_loading_scores[i]) + "\n")
-    file.write(text)
-file.close()
-
-print(datetime.now().strftime("%H:%M:%S>"), "Script terminated successfully")
 
 # %% Diagnostics
 
