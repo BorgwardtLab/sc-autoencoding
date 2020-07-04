90a2dfb84eb2dc55839cfc3aac9041611e462cb0
unknown <sstreib@student.ethz.ch>
2020-07-04 18:18:54 +0200

    added the run all bash script

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 conda activate tf
-python 1_Main_Scripts/sca_datamerger.py
-python 2_Baseline_Scripts/sca_PCA.py
-python 2_Baseline_Scripts/sca_LSA.py
-python 2_Baseline_Scripts/sca_Isomap.py
-python 2_Baseline_Scripts/sca_tSNE.py
-python 2_Baseline_Scripts/sca_UMAP.py
+python 1_Main_Scripts/sca_datamerger.py 2>&1 run_all.log
+python 2_Baseline_Scripts/sca_PCA.py 2>&1 run_all.log
+python 2_Baseline_Scripts/sca_LSA.py 2>&1 run_all.log
+python 2_Baseline_Scripts/sca_Isomap.py 2>&1 run_all.log
+python 2_Baseline_Scripts/sca_tSNE.py 2>&1 run_all.log
+python 2_Baseline_Scripts/sca_UMAP.py 2>&1 run_all.log
