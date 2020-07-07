6a55bb905fe09b55cac12f2c8349a02262a229f2
unknown <sstreib@student.ethz.ch>
2020-07-07 22:42:44 +0200

    had wrong command in runall.sh

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 
 python 1_Main_Scripts/sca_datamerger.py |& tee -a run_all.log
 
-Rscript sca_preprocessing.R ../inputs/raw_input_combined/filtered_matrices_mex/hg19/ ../inputs/preprocessed_data/ ../outputs/preprocessed_data/ 200 1750 5 2000 |& tee -a run_all.log
+Rscript 1_Main_Scripts/sca_preprocessing.R ../inputs/raw_input_combined/filtered_matrices_mex/hg19/ ../inputs/preprocessed_data/ ../outputs/preprocessed_data/ 200 1750 5 2000 |& tee -a run_all.log
 
 python 2_Baseline_Scripts/sca_PCA.py |& tee -a run_all.log
 python 2_Baseline_Scripts/sca_LSA.py |& tee -a run_all.log
