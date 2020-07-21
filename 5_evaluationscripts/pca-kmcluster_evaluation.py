10612173bffc57330d5801201d9fef0a444c8ea1
unknown <sstreib@student.ethz.ch>
2020-07-21 15:13:49 +0200

    expanded hpyerparam

--- 5_evaluationscripts/pca-kmcluster_evaluation.py
+++ 5_evaluationscripts/pca-kmcluster_evaluation.py
@@ -6,18 +6,8 @@ Created on Sat Jul 18 20:03:52 2020
 """
 
 # %%
-
 import sys
-sys.path.insert(1, "../2_Baseline_Scripts")
-from sca_PCA import sca_PCA
-sys.path.insert(1, "../1_Main_Scripts")
-from sca_kmcluster import sca_kmcluster
-
-
-import matplotlib.pyplot as plt
 import os
-import numpy as np
-
 
 try:
     os.chdir(os.path.dirname(sys.argv[0]))
@@ -25,11 +15,25 @@ except:
     pass
    
 
+sys.path.insert(1, "../2_Baseline_Scripts")
+from sca_PCA import sca_PCA
+sys.path.insert(1, "../1_Main_Scripts")
+from sca_kmcluster import sca_kmcluster
+
+
+import matplotlib.pyplot as plt
+import statistics
+import numpy as np
+
+
+
+
      
 # %% 
-
 num_cluster = 5
- 
+repetitions = 100
+
+
 try:
     num_clusters = sys.argv[1]
     print("default k for kmcluster (5) overwritten through command line ({0:d}).".format(num_cluster))
@@ -39,8 +43,12 @@ except:
              
 
 
+
 componentslist = [5, 10, 15, 20, 30, 40, 50, 60, 75, 100]
-puritylist = np.zeros(len(componentslist))
+
+purities = np.zeros((len(componentslist), repetitions))
+errorbars = np.zeros(len(componentslist))
+
 
 input_dir = "../inputs/preprocessed_data/"
 output_dir = "../outputs/hyperparameter/scaPCA_output/"
@@ -62,20 +70,26 @@ for i in range(len(componentslist)):
             output_dir = intermediate_dir,
             outputplot_dir = intermediate_dir)
     
-    current_purity = sca_kmcluster(k = num_cluster,
-                  dimensions = 0,
-                  input_path = intermediate_dir,
-                  output_dir = intermediate_dir,
-                  outputplot_dir = intermediate_dir,
-                  verbosity = 0,
-                  elbow = False,
-                  title = "PCA with {0:d} components".format(numcomp),
-                  reset = True)
     
-    puritylist[i] = current_purity
+    for j in range(repetitions):
+        current_purity = sca_kmcluster(k = num_cluster,
+                      dimensions = 0,
+                      input_path = intermediate_dir,
+                      output_dir = intermediate_dir,
+                      outputplot_dir = intermediate_dir + str(j),
+                      verbosity = 0,
+                      elbow = False,
+                      title = "PCA with {0:d} components".format(numcomp),
+                      reset = False)
     
-    plt.close('all')
+        purities[i, j] = current_purity
         
+        plt.close('all')
+        
+        
+        
+    errorbars[i] = statistics.stdev(purities[i,:])
+
         
 
 
@@ -83,9 +97,22 @@ for i in range(len(componentslist)):
 # %%
 
 
+averages = np.average(purities, axis = 1)
+
 plt.figure()
-plt.plot()
-plt.plot(componentslist, puritylist, "r-")
+for i, (x,y) in enumerate(zip(componentslist, averages)):
+    
+    label = "{:.5f}".format(y)
+    
+    plt.annotate(label, # this is the text
+             (x,y), # this is the point to label
+             textcoords="offset points", # how to position the text (if xytesxt is points or pixels)
+             xytext=(30,5), # distance from text to points (x,y)
+             ha='center') # horizontal alignment can be left, right or center
+    
+    plt.errorbar(x,y,yerr= errorbars[i])
+
+plt.plot(componentslist, averages, linestyle = "-", marker = "o")
 plt.title("global purities for different number of PCs")
 plt.xlabel("number of principal components calculated")
 plt.ylabel("global purity evaluated by kmcluster")
@@ -94,8 +121,7 @@ plt.savefig(output_dir + "puritygraph.png")
 
 
 
-
-
+np.savetxt(output_dir + "purities.csv", purities, delimiter = "\t")
 
 
 
