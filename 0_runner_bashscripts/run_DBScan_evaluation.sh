bb983bda5ff866c66028a206e6a141f0d2ac4925
unknown <sstreib@student.ethz.ch>
2020-08-14 04:28:43 +0200

    hotfix for the knee drawing epssearch

--- 0_runner_bashscripts/run_DBScan_evaluation.sh
+++ 0_runner_bashscripts/run_DBScan_evaluation.sh
@@ -6,7 +6,7 @@ rm log_run_DBScan_evaluation.log
 
 
 #for epsearch
-python ../5_Optimization/dbscan_epssearch_knn.py --num_components 2 3 4 5 6 7 8 9 10 15 20 30 40 50 60 80 100 --num_kmclusters 5 --reps 100 --input_dir "../inputs/data/preprocessed_data/" --output_dir "../outputs/Optimization/num_PCA/" |& tee -a log_run_DBScan_evaluation.log
+python ../5_Optimization/dbscan_epssearch_knn.py --input_dir "../inputs/data/preprocessed_data/" --output_dir "../outputs/Optimization/num_PCA/" |& tee -a log_run_DBScan_evaluation.log
 date
 echo "finished the epsearch"
 
