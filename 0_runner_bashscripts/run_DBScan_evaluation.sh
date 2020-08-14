78a17518df36675809cd12876cc5185275863a5c
unknown <sstreib@student.ethz.ch>
2020-08-14 03:05:33 +0200

    hotfix

--- 0_runner_bashscripts/run_DBScan_evaluation.sh
+++ 0_runner_bashscripts/run_DBScan_evaluation.sh
@@ -5,11 +5,16 @@
 rm log_run_DBScan_evaluation.log
 
 
-#for PCA
-python ../5_Optimization/dbscan_epssearch_knn.py --num_components 2 3 4 5 6 7 8 9 10 15 20 30 40 50 60 80 100 --num_kmclusters 5 --reps 100 --input_dir "../inputs/data/preprocessed_data/" --output_dir "../outputs/Optimization/num_PCA/" 
+#for epsearch
+python ../5_Optimization/dbscan_epssearch_knn.py --num_components 2 3 4 5 6 7 8 9 10 15 20 30 40 50 60 80 100 --num_kmclusters 5 --reps 100 --input_dir "../inputs/data/preprocessed_data/" --output_dir "../outputs/Optimization/num_PCA/" |& tee -a log_run_DBScan_evaluation.log
+date
+echo "finished the epsearch"
 
 
-|& tee -a log_run_DBScan_evaluation.log
+#gridsearch
+python ../5_Optimization/dbscan_gridsearch_pca.py --eps 10 12.5 15 17.5 20 25 30 35 40 45 50 --minpts 1 2 3 4 5 6 7 8 9 10 15 20 30 50 100 --input_dir "../inputs/baselines/baseline_data/scaPCA_output/" --output_dir "../outputs/optimization/dbscan_gridsearch/" |& tee -a log_run_DBScan_evaluation.log
+date
+echo "finished the gridsearch"
 
 
 
