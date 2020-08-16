95b5ba8c218bdb967d314720134eeb486bc6bd68
unknown <sstreib@student.ethz.ch>
2020-08-17 00:41:10 +0200

    hotfix

--- 3_Autoencoder/sca_autoencoder.py
+++ 3_Autoencoder/sca_autoencoder.py
@@ -33,7 +33,7 @@ from keras.objectives import mean_squared_error
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 
-from keras.objectives import mse, mae, mape, msle, squared_hinge, hinge, binary_crossentropy, categorical_crossentropy, sparse_categorical_crossentropy, kld, poisson, cosine_proximity
+#from keras.objectives import mse, mae, mape, msle, squared_hinge, hinge, binary_crossentropy, categorical_crossentropy, sparse_categorical_crossentropy, kld, poisson, cosine_proximity
 
 
 import numpy as np
