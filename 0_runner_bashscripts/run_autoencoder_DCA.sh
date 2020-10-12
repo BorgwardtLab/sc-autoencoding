b69bd0ef4fa740a486af98b32f6cb76924fd8c5a
unknown <sstreib@student.ethz.ch>
2020-10-12 19:56:46 +0200

    ydfas

--- 0_runner_bashscripts/run_autoencoder_DCA.sh
+++ 0_runner_bashscripts/run_autoencoder_DCA.sh
@@ -13,7 +13,7 @@ rm $filename
 # python ../1_Processing/sca_datamerger.py --mode both --input_dir "../inputs/data/raw_input" --output_dir "../inputs/data/raw_input_combined" |& tee -a $filename
 
 # PREPROCESSING
-python ../1_Processing/sca_countdata_preprocessor.py --mingenes 200 --mincells 1 --maxfeatures 1500 --maxmito 5 --features 2000 --test_fraction 0.25 --input_dir "../inputs/data/raw_input_combined/filtered_matrices_mex/hg19/" --output_dir "../inputs/sca/sca_preprocessed_data/" --verbosity 0 |& tee -a $filename
+# python ../1_Processing/sca_countdata_preprocessor.py --mingenes 200 --mincells 1 --maxfeatures 1500 --maxmito 5 --features 2000 --test_fraction 0.25 --input_dir "../inputs/data/raw_input_combined/filtered_matrices_mex/hg19/" --output_dir "../inputs/sca/sca_preprocessed_data/" --verbosity 0 |& tee -a $filename
 
 
 
