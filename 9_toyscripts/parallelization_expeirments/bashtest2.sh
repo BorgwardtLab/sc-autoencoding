15b1a4a114b9b16887429ebf89a759d7e007acdb
unknown <sstreib@student.ethz.ch>
2020-10-18 22:08:18 +0200

    adsfs

---
+++ 9_toyscripts/parallelization_expeirments/bashtest2.sh
@@ -0,0 +1,25 @@
+
+
+
+
+for bla in gli glu gla
+do 
+python script_word.py $bla first
+python script_word.py $bla second
+python script_word.py $bla third &
+done
+
+wait
+
+
+
+
+
+python script1.py
+
+
+
+
+
+
+
--- 9_toyscripts/parallelization_expeirments/script_word.py
+++ 9_toyscripts/parallelization_expeirments/script_word.py
@@ -9,27 +9,27 @@ Created on Sun Oct 18 20:37:03 2020
 import time
 import sys
 # call script with python script.py var1 var2
+import random
 
 
 
 z = 20
-number = sys.argv[1]
+ele = sys.argv[1]
+ele2 = sys.argv[2]
+
 
 
 for i in range(z):
 
-    
-    
     with open("test.tsv", "a") as outfile:
-        outfile.write(str(number))
+        outfile.write(str(ele) + "\t" + str(ele2))
         outfile.write("\n")
+        
+    sleepytime = random.uniform(0.00001, 20) 
     time.sleep(1)
     
     
-    
-print("I ended " + str(number))    
-    
-    
+
     
 
 
