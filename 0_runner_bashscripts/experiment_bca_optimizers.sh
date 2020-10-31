d54c7525974f271992ba7deebb1adc17b2c0828d
unknown <sstreib@student.ethz.ch>
2020-10-31 04:48:41 +0100

    fasdfds

---
+++ 0_runner_bashscripts/experiment_bca_optimizers.sh
@@ -0,0 +1,64 @@
+
+source ~/anaconda3/etc/profile.d/conda.sh # to source the conda command. Check directory if it doesn't work.
+conda activate tf
+printf "[BCA experiment optimizer]" 
+conda env list	# it should be visible in the log-textfile. I'm not saving it to anything else.
+
+
+
+mkdir logs
+logfile="logs/6_exBCA_optimizer.log"
+
+preprocessed_ctdata="../inputs/data/preprocessed_data_autoencoder/"
+outdir="../outputs/experiments/optimizer/"
+
+
+optimizers=("SGD" "RMSprop" "Adam" "Adadelta" "Adagrad" "Adamax" "Nadam" "Ftrl")
+optimizers=("SGD" "RMSprop")
+
+
+
+
+start=`date +%s`
+printf "############################################################################\n################### " &>> $logfile
+echo -n START: `date` |& tee -a $logfile
+printf " ###################\n############################################################################\n\n" &>> $logfile
+
+
+(
+for optimizer in ${optimizers[@]}; do
+echo $loss
+
+	(
+	python ../3_Autoencoder/bca_autoencoder.py --mode complete --loss poisson --activation relu --optimizer $optimizer --input_dir $preprocessed_ctdata --output_dir "${outdir}bca_data/${loss}/" --outputplot_dir "${outdir}bca_data/${loss}/"  |& tee -a $logfile
+	
+	(
+	python ../4_Evaluation/sca_kmcluster.py --title ${loss} --k 8 --limit_dims 0 --verbosity 0 --input_dir "${outdir}bca_data/${loss}/" --output_dir "${outdir}cluster_result/" |& tee -a $logfile
+	) & (
+	python ../4_Evaluation/sca_randforest.py --title ${loss} --n_trees 100 --input_dir "${outdir}bca_data/${loss}/" --output_dir "${outdir}randomforest_result/" |& tee -a $logfile
+	)
+	
+	
+	)
+
+
+done
+wait
+)
+
+
+(
+python ../4_Evaluation/visualize.py  --title "BCAopti"  --output_dir ${outdir} --random_forest_results "${outdir}randomforest_result/" --kmcluster_results "${outdir}cluster_result/" |& tee -a $logfile
+)
+
+
+
+end=`date +%s`
+printf "\nBCA experiment optimizer took %d minutes\n" `echo "($end-$start)/60" | bc` |& tee -a $logfile
+printf "\n################### " &>> $logfile
+echo -n DONE: `date` |& tee -a $logfile
+printf " ####################\n############################################################################\n\n\n\n\n\n" &>> $logfile
+)
+
+
+
