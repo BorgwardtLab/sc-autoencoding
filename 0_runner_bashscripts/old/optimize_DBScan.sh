e4fe9988f25ded0e99f34bc3a79505407d4cbc6e
unknown <sstreib@student.ethz.ch>
2020-10-31 15:00:30 +0100

    fadsf

--- 0_runner_bashscripts/old/optimize_DBScan.sh
+++ 0_runner_bashscripts/old/optimize_DBScan.sh
@@ -1,6 +1,6 @@
 
 mkdir logs
-filename=logs/log_optimize_DBScan.log
+filename=logs/log_optimize_DBScan_gridsearch.log
 
 rm $filename
 
@@ -13,8 +13,8 @@ echo "finished the epsearch"
 
 
 #gridsearch
-#python ../5_Optimization/dbscan_gridsearch_pca.py --eps 10 12.5 15 17.5 20 25 30 35 40 45 50 --minpts 1 2 3 4 5 6 7 8 9 10 15 20 30 50 100 --input_dir "../inputs/baselines/baseline_data/scaPCA_output/" --output_dir "../outputs/optimization/dbscan_gridsearch/" |& tee -a $filename
-python ../5_Optimization/dbscan_gridsearch_pca.py --eps 10 15 20 35 50 75 100 --minpts 1 4 10 100 --input_dir "../inputs/baselines/baseline_data/scaPCA_output/" --output_dir "../outputs/optimization/dbscan_gridsearch/" |& tee -a $filename
+
+python ../5_Optimization/dbscan_gridsearch_pca.py --eps 10 15 17.5 20 22.5 25 27.5 30 32.5 35 40 45 50 --minpts 1 2 3 4 5 6 8 10 20 50 --input_dir "../inputs/baselines/baseline_data/scaPCA_output/" --output_dir "../outputs/optimization/dbscan_gridsearch/" |& tee -a $filename
 
 date
 echo "finished the gridsearch"
---
+++ 0_runner_bashscripts/optimizee_dbscan_gridsearch.sh
@@ -0,0 +1,33 @@
+
+mkdir logs
+filename=logs/log_optimize_DBScan_gridsearch.log
+
+rm $filename
+
+
+
+#for epsearch
+#python ../5_Optimization/dbscan_epssearch_knn.py --input_dir "../inputs/data/preprocessed_data/" --outputplot_dir "../outputs/optimization/dbscan_epsearch/" |& tee -a $filename
+date
+echo "finished the epsearch"
+
+
+#gridsearch
+
+#python ../5_Optimization/dbscan_gridsearch_pca.py --eps 10 15 17.5 20 22.5 25 27.5 30 32.5 35 40 45 50 --minpts 1 2 3 4 5 6 8 10 20 50 --input_dir "../inputs/baseline_data/scaPCA_output/" --output_dir "../outputs/optimization/technique_evaluation/dbscan_gridsearch/" |& tee -a $filename
+
+python ../5_Optimization/dbscan_gridsearch_pca.py --eps 10 20 30 --minpts 2 5 10 --input_dir "../inputs/baseline_data/scaPCA_output/" --output_dir "../outputs/optimization/technique_evaluation/dbscan_gridsearch/" |& tee -a $filename
+
+
+
+date
+echo "finished the gridsearch"
+
+
+
+echo "I have finished running the DBScan"
+
+
+
+
+
