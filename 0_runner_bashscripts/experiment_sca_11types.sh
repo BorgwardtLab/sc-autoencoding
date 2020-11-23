a0c22cc5736697bf3904d4e4ee544b78971d6321
unknown <sstreib@student.ethz.ch>
2020-11-23 22:08:32 +0100

    hotfix

--- 0_runner_bashscripts/experiment_sca_11types.sh
+++ 0_runner_bashscripts/experiment_sca_11types.sh
@@ -8,9 +8,14 @@ conda env list	# it should be visible in the log-textfile. I'm not saving it to 
 
 
 
-# AEtypes=("normal" "poisson" "nb" "nb-conddisp" "nb-shared" "nb-conddisp" "nb-fork" "zinb-shared" "zinb-elempi" "zinb-conddisp" "zinb-fork")
-# AEtypes=("normal" "poisson" "nb")
-AEtypes=("nb-conddisp" "nb-shared" "nb-conddisp" "nb-fork" "zinb-shared" "zinb-elempi" "zinb-conddisp" "zinb-fork")
+
+# AEtypes=("normal" "poisson" "nb" "nb-conddisp" "nb-shared" "nb-fork" "zinb" "zinb-conddisp" "zinb-shared" "zinb-fork" "zinb-elempi")
+
+
+# worked ones: nb normal poisson zinbconddisp zinbfork
+
+
+AEtypes=("nb-conddisp" "nb-shared" "nb-fork" "zinb-shared" "zinb-elempi" "zinb")
 
 
 
